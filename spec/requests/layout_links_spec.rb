e6a2d2a60f1c06c20d58741699d4b3cfa98bfb1a
keetaek <keetaek@gmail.com>
2011-01-23 22:42:48 -0800

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -24,5 +24,9 @@ describe "LayoutLinks" do
       response.status.should be(200)
       response.should have_selector('title', :content => "Help")
     end
+    it "should have a signup page at 'signup'" do
+      get '/signup'
+      response.should have_selector('title', :content => 'Sign up')
+    end
   end
 end
