f3f159019f9dd260b1db2c5fea82d7c9fcc06c30
keetaek <keetaek@gmail.com>
2011-01-26 15:46:41 -0800

    chapter8 exercise

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -48,6 +48,22 @@ describe UsersController do
       get :new
       response.should have_selector("title", :content => "Sign up")
     end
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]']")
+    end
+    it "should have a email field" do
+      get :new
+      response.should have_selector("input[name='user[email]']")
+    end
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]']")
+    end
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]']")
+    end
   end
   describe "POST 'create'" do
     before(:each) do
