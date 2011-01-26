ebb8fa4f0be64ae00fa2bd859f7bcf715fbebe0e
keetaek <keetaek@gmail.com>
2011-01-26 10:15:17 -0800

    removed ruby-debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,6 @@ class User < ActiveRecord::Base
   def has_password?(submitted_password)
     self.encrypted_password == encrypt(submitted_password)
   end
-
   def self.authenticate (email, submitted_password)
     user = find_by_email(email)
     return nil if user.nil?
