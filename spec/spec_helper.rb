6afd152f1895e0ba57977fc3fa2d897dd10e1baf
keetaek <keetaek@gmail.com>
2011-01-27 14:18:22 -0800

    chapter9 ex1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,4 +27,10 @@ RSpec.configure do |config|
   def test_sign_in(user)
     controller.sign_in(user)
   end
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,       :with => user ? user.email : ""
+    fill_in :password,    :with => user ? user.password : ""
+    click_button
+  end
 end
